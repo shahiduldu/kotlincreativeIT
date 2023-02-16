42ef201d184cf2610dc6a614447131761e456994
shahiduldu <shahidul3002@gmail.com>
2023-02-16 16:37:31 +0600

    question

---
+++ app/src/main/java/com/example/qiuz1class_19/QuestionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.qiuz1class_19
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+    }
+}
